ec4c157e3d829edafc393a6450b5df3316500b27
Misha36bm <47403281+Misha36bm@users.noreply.github.com>
2023-05-24 21:19:50 +0300

    bootstrap app script

--- public/index.php
+++ public/index.php
@@ -1,3 +1,20 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
+use Panda\Tz\Kernel;
+use Whoops\Run;
+use Whoops\Handler\PrettyPageHandler;
+
+$whoops = new Run;
+$whoops->pushHandler(new PrettyPageHandler);
+$whoops->register();
+
+$app  = new Kernel(dirname(__DIR__));
+
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+echo $app
+    ->getRouter()
+    ->dispatch($httpMethod, $uri);
