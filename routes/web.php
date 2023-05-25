cfbc99f17d70e80c9a73b6ca67db3348b5050fe4
Misha36bm <47403281+Misha36bm@users.noreply.github.com>
2023-05-26 00:07:15 +0300

    add route for main page

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,39 @@
 <?php
 
+use Panda\Tz\Controllers\HomeController;
+
 return [
     /**
- * Template for routes: 
- * 
- * For call function from class:
- * [
- *  'methood' => 'get',
- *  'url' => '/',
- *  'callback' => [TestController::class, 'index']
- * ]
- * 
- * For call callback:
- * [
- *     'methood' => 'get',
- *     'url' => '/test',
- *     'callback' => function () {
- *         return 'Hello world';
- *     }
- * ]
- * 
- * For call function by it name:
- * 
- * [
- *    'methood' => 'get',
- *    'url' => '/abc',
- *    'callback' => 'phpinfo'
- * ]
- */
+     * Template for routes: 
+     * 
+     * For call function from class:
+     * [
+     *  'methood' => 'get',
+     *  'url' => '/',
+     *  'callback' => [TestController::class, 'index']
+     * ]
+     * 
+     * For call callback:
+     * [
+     *     'methood' => 'get',
+     *     'url' => '/test',
+     *     'callback' => function () {
+     *         return 'Hello world';
+     *     }
+     * ]
+     * 
+     * For call function by it name:
+     * 
+     * [
+     *    'methood' => 'get',
+     *    'url' => '/abc',
+     *    'callback' => 'phpinfo'
+     * ]
+     */
+
+    [
+        'methood' => 'get',
+        'url' => '/',
+        'callback' => [HomeController::class, 'index']
+    ]
 ];
