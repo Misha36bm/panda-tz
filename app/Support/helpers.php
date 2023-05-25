89e66c3a209efc8261eaa7e3c82ce511094c9240
Misha36bm <47403281+Misha36bm@users.noreply.github.com>
2023-05-26 00:32:56 +0300

    create view functoin for print pages

--- app/Support/helpers.php
+++ app/Support/helpers.php
@@ -1,6 +1,17 @@
 <?php
 
-function test_func()
+function view($view = null, $data = [])
 {
-    dd(222222);
+    $pathToViewFolder = app()->getAppPath() . '/resources/views/';
+
+    $path = $pathToViewFolder . $view . '.php';
+
+    if (!file_exists($path)) {
+        throw new Exception('Template not found: ' . $view);
+    }
+
+    extract($data);
+    ob_start();
+    include $path;
+    return ob_get_clean();
 }
