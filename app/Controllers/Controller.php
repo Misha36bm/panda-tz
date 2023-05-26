e1b3c222ee84fe0499926e1e649942cc79096f22
Misha36bm <47403281+Misha36bm@users.noreply.github.com>
2023-05-26 23:25:47 +0300

    create requst variable for main controller

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -4,4 +4,14 @@ namespace Panda\Tz\Controllers;
 
 class Controller
 {
+    protected $request = [];
+
+    public function __construct()
+    {
+
+        $getData = $_GET;
+        $postData = $_POST;
+
+        $this->request = array_merge($getData, $postData);
+    }
 }
