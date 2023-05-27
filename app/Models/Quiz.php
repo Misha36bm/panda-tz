240aeecab2ed52647b3bcecb6a4dc96628c80151
Misha36bm <47403281+Misha36bm@users.noreply.github.com>
2023-05-27 11:15:31 +0300

    update Quiz model
    create function for update quiz title
    
    add function for update options of quiz
    
    remove timestamps in table for model
    
    fix fillable name for title

--- app/Models/Quiz.php
+++ app/Models/Quiz.php
@@ -11,8 +11,10 @@ class Quiz extends Model
 
     protected $fillable = [
         'user_id',
-        'title',
+        'quiz_title',
     ];
+
+    public $timestamps = false;
 
     /**
      * Get the user that owns the Quiz
@@ -32,5 +34,32 @@ class Quiz extends Model
     public function options(): HasMany
     {
         return $this->hasMany(QuizOptions::class, 'quiz_id', 'id');
+    }
+
+    public function updateTitle($title)
+    {
+        $this->update(['quiz_title' => $title]);
+
+        return $this;
+    }
+
+    public function updateOptions($options, $correctAnswerIndex)
+    {
+        $this->options->each(fn ($item) => $item->deleteOption());
+
+        $optionsModelsToSave = [];
+
+        foreach ($options as $index => $optionText) {
+            $optionsModelsToSave[$index] = new QuizOptions([
+                'quiz_id' => $this->id,
+                'option_text' => $optionText,
+                'is_correct' => $index == $correctAnswerIndex
+            ]);
+        }
+
+        $this->options()->saveMany($optionsModelsToSave);
+
+
+        return $this;
     }
 }
